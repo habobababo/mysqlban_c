f6cfe03f4c663749741c6654976cfaedd3856a8b
habobababo <shadowman11221@gmail.com>
2017-08-28 13:28:15 +0200

    renew
    renew

--- lua/autorun/server/sv_main.lua
+++ lua/autorun/server/sv_main.lua
@@ -67,117 +67,20 @@ local function Core_AddBan( calling_ply, steamid, nick, minutes, reason )
 	if !reason then reason = "" end
 	if !nick then nick = "" end
 	local steamid64 = util.SteamIDTo64(steamid)
-
+	print(nick..", "..steamid64..", "..minutes..", "..reason..", "..nick..", "..server)
 	bansquery("INSERT INTO bans (steamid64, name, minutes, reason, admin, server ) VALUES ('"..nick.."', "..steamid64.."', "..minutes..", '"..reason.."', '"..nick.."', "..server..") ")
 end
 
-function ULib.addBan( steamid, time, reason, name, admin )
-	if reason == "" then reason = nil end
-
-	local admin_name
-	local admin_nick = "ADMIN"
-
-	if admin then
-		admin_name = "(Console)"
-		admin_nick = "(Console)"
-		if admin:IsValid() then
-			admin_name = string.format( "%s(%s)", admin:Name(), admin:SteamID() )
-			admin_nick = admin:Name()
-		end
-	end
-
-	local t = {}
-	local timeNow = os.time()
-	if ULib.bans[ steamid ] then
-		t = ULib.bans[ steamid ]
-		t.modified_admin = admin_name
-		t.modified_time = timeNow
-	else
-		t.admin = admin_name
-	end
-	t.time = t.time or timeNow
-	if time > 0 then
-		t.unban = ( ( time * 60 ) + timeNow )
-	else
-		t.unban = 0
-	end
-	if reason then
-		t.reason = reason
-	end
-	if name then
-		t.name = name
-	end
-	ULib.bans[ steamid ] = t
-
-	local strTime = time ~= 0 and ULib.secondsToStringTime( time*60 )
-	local shortReason = "Banned for " .. (strTime or "eternity")
-	if reason then
-		shortReason = shortReason .. ": " .. reason
-	end
-
-	local longReason = shortReason
-	if reason or strTime or admin then -- If we have something useful to show
-		longReason = "\n" .. ULib.getBanMessage( steamid ) .. "\n" -- Newlines because we are forced to show "Disconnect: <msg>."
-	end
-
-
-	local ply_nick = ""
-
-	local ply = player.GetBySteamID( steamid )
-	if ply then
-		ULib.kick( ply, longReason, nil, true)
-		ply_nick = ply:Name()
-	end
-
-	-- Remove all semicolons from the reason to prevent command injection
-	shortReason = string.gsub(shortReason, ";", "")
-	Core_AddBan(admin_name, steamid, ply_nick, time, reason )
-	-- This redundant kick code is to ensure they're kicked -- even if they're joining
-	game.ConsoleCommand( string.format( "kickid %s %s\n", steamid, shortReason or "" ) )
-	game.ConsoleCommand( string.format( "banid %f %s kick\n", time, steamid ) )
-	game.ConsoleCommand( "writeid\n" )
-
-	ULib.fileWrite( ULib.BANS_FILE, ULib.makeKeyValues( ULib.bans ) )
-	hook.Call( ULib.HOOK_USER_BANNED, _, steamid, t )
+local function Core_Banfunction( steamid, data )
+	Core_AddBan( data.admin, util.SteamIDTo64(steamid), data.name, data.unban - data.time, data.reason )
 end
+hook.Add("ULibPlayerBanned", "Core_Banfunction", Core_Banfunction)
 
-function ULib.kick( ply, reason, calling_ply )
-	local nick = calling_ply and calling_ply:IsValid() and
-		(string.format( "%s(%s)", calling_ply:Nick(), calling_ply:SteamID() ) or "Console")
-	local steamid = ply:SteamID()
-	if reason and nick then
-		ply:Kick( string.format( "Kicked by %s - %s", nick, reason ) )
-		Core_AddBan(nick, steamid, nick, 1, reason )
-	elseif nick then
-		ply:Kick( "Kicked by " .. nick )
-		Core_AddBan(nick, steamid, ply, 1, " " )
-	else
-		ply:Kick( reason or "[ULX] Kicked from server" )
+local function Core_Kickfunction( steamid, reason, caller )
+	local clr = ""
+	if caller then
+		clr = caller:Nick();
 	end
-	hook.Call( ULib.HOOK_USER_KICKED, _, steamid, reason or "[ULX] Kicked from server", calling_ply )
+	Core_AddBan( clr, util.SteamIDTo64(steamid), "", 1, reason)
 end
-
-if server == 3 then
-	local reason = "Karma too low"
-	function KARMA.CheckAutoKick(ply)
-		if ply:GetBaseKarma() <= config.kicklevel:GetInt() then
-			if hook.Call("TTTKarmaLow", GAMEMODE, ply) == false then
-				return
-			end
-			ServerLog(ply:Nick() .. " autokicked/banned for low karma.\n")
-			ply.karma_kicked = true
-			if config.persist:GetBool() then
-				local k = math.Clamp(config.starting:GetFloat() * 0.8, config.kicklevel:GetFloat() * 1.1, config.max:GetFloat())
-				ply:SetPData("karma_stored", k)
-				KARMA.RememberedPlayers[ply:SteamID()] = k
-			end
-			if config.autoban:GetBool() then
-				ply:KickBan(config.bantime:GetInt(), reason)
-				Core_AddBan("CONSOLE", ply:SteamID(),ply:Nick(), config.bantime:GetInt(), reason )
-			else
-				ply:Kick(reason)
-				Core_AddBan("CONSOLE", ply:SteamID(),ply:Nick(), 1, reason )
-			end
-		end
-	end
-end
+hook.Add("ULibPlayerKicked", "Core_Kickfunction", Core_Kickfunction)
